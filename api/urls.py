4fa362fd959a5f30c38d2c9919a1e83de2395215
Matin.h.f81 <matinhosseini795@gmail.com>
2023-02-18 02:51:03 +0330

    Create api/HomeApiView ✅

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import HomeApiView
 
+app_name = 'api'
 
 urlpatterns = [
-
+    path('home/', HomeApiView.as_view(), name='home-api')
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core import serializers
 
-# Create your views here.
+from unit_selection.models import Departemant
+
+class HomeApiView(APIView):
+
+    def get(self, request):
+        departemants = {}
+        for departemant in Departemant.objects.all():
+            departemants[f"{departemant.name}"] = json.loads(serializers.serialize('json', departemant.courses.filter(
+                status=True).order_by("-credits", "name")))
+
+        print(departemants)
+        notice = request.session.get("notice")
+        return Response(departemants)
