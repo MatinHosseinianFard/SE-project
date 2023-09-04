22db89a50a7035123a73bced7d4861ced6b9ecb0
MatinHosseinianFard <matinhosseini795@gmail.com>
2023-09-04 20:34:41 +0330

    Add Analytics ✅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { inject } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 import { onStart } from "./store/store.js";
 
@@ -21,16 +21,18 @@ import About from "./pages/About/About.js";
 const App = () => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.isAuthenticated);
-  inject();
+  
   useEffect(() => {
     dispatch(onStart());
   });
 
   return (
     <Container>
+      <Analytics />
       <Router>
         {!isAuthenticated ? (
           <Routes>
+            <Analytics />
             <Route path="/" element={<Login/>} exact />
             <Route path="/signup" element={<Signup/>} exact />
           </Routes>
@@ -38,6 +40,7 @@ const App = () => {
           <Container>
             <Header/>
             <Routes>
+            <Analytics />
               <Route path="/" element={<Home/>} exact />
               <Route path="/suggest" element={<Suggest/>} exact />
               <Route path="/favourite" element={<Favourite/>} exact />
